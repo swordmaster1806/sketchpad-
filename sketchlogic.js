e8240fe77b3fcc0edc3e35ed3e75eefd4c68de70
swordmaster1806 <265546703+swordmaster1806@users.noreply.github.com>
2026-08-17 16:55:41 +0530

    mvp rgb sketchpad with erase , different modes and reset

--- sketchlogic.js
+++ sketchlogic.js
@@ -1,16 +1,13 @@
 let mainContainer = document.querySelector("#container");
-
-makeASquare()
-
-
 let clickModeActive = false;
 let erase = false;
 let mousedown = false;
 
+makeASquare()
 
 
 
-function paint(square) {
+function paintASquare(square) {
 
   let rgb = ["red", "green", "blue"];
   let randomRgbIndex = Math.floor(Math.random() * (rgb.length));
@@ -32,7 +29,7 @@ function makeASquare() {
 
       let newSquare = document.createElement("div");
       rowContainer.appendChild(newSquare);
-      newSquare.classList.add("rowFlexBoxChildren");
+      newSquare.classList.add("squares");
 
     }
 
@@ -43,30 +40,62 @@ function makeASquare() {
 
 
 
-mainContainer.addEventListener("mouseover", function(square){
+mainContainer.addEventListener("mouseover", function (square) {
 
-  if((!clickModeActive || mousedown) && square.target !== mainContainer) paint(square);
-     
+  if ((!clickModeActive || mousedown) && square.target !== mainContainer) paintASquare(square);
+
 });
 
-mainContainer.addEventListener("mousedown" , function(square){
+mainContainer.addEventListener("mousedown", function (square) {
+
   square.preventDefault();
-  if(clickModeActive && square.target !== mainContainer){
-    paint(square);
+  if (clickModeActive && square.target !== mainContainer) {
+    paintASquare(square);
     mousedown = true;
   }
 })
 
-document.body.addEventListener("mouseup" , () =>{
+document.body.addEventListener("mouseup", () => {
 
-   mousedown = false
-   console.log("mouse was just put up");
+  mousedown = false;
+  console.log("mouse was just put up");
+
+})
+
+
+
+let squareList = document.querySelectorAll(".squares");
+
+
+document.body.addEventListener("click", function (event) {
+
+  if (event.target.id === "reset")
+    squareList.forEach((square) => square.style.backgroundColor = "white");
+
+
+  else if (event.target.id === "mode") {
+    clickModeActive = !clickModeActive
+
+    if(event.target.textContent === "trace") 
+      event.target.textContent = "click";
+
+    else event.target.textContent = "trace";
+
+  }
   
-  })
+  else if (event.target.id === "eraser") {
 
-document.querySelector("#mode").addEventListener("click" , () => {
-  clickModeActive = !clickModeActive
+    erase = !erase;
 
-});
+    if (event.target.textContent === "eraser (on)") 
+      event.target.textContent = "eraser (off)";
+    
+
+    else 
+      event.target.textContent = "eraser (on)";
+ 
+  
+  }
 
 
+})
